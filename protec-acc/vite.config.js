8b8862011a80d0f4710487256a057fa48794cfae
aeschalna <aeschalna@gmail.com>
2024-09-05 21:19:39 -0700

    back to working code, start here

--- protec-acc/vite.config.js
+++ protec-acc/vite.config.js
@@ -3,16 +3,16 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/protec-acc/",
+  // base: "/protec-acc/",
   build: {
     outDir: "dist",
-    rollupOptions: {
-      external: ["/protec-acc/src/main.jsx"],
-    },
+    // rollupOptions: {
+    //   external: ["/protec-acc/src/main.jsx"],
+    // },
   },
   plugins: [react()],
-  server: {
-    port: 3000,
-    open: true,
-  },
+  // server: {
+  //   port: 3000,
+  //   open: true,
+  // },
 });
